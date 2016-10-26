9d7f6fa174279eb1ea3ef872bfe2d9d9f83f4b6c
Jacob Hixon <jacobhixon47@gmail.com>
2016-10-25 23:47:28 -0700

    Add function that returns false.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
+var leapYear = function(year) {
+  if (year % 4 === 0) {
+    return true;
+  }
+  else {
+  return false;
+  }
+};
+
+
 $(document).ready(function() {
   $("form#leap-year").submit(function() {
     event.preventDefault();
     var year = parseInt($("input#year").val());
-    var result = leapYear(year)
+    var result = leapYear(year);
     $("#result").text(result);
   });
 });
