071987c7d321dc3a6196584bcaea22606d565169
Jacob Hixon <jacobhixon47@gmail.com>
2016-10-26 00:04:19 -0700

    Add user int logic conditionals to display custom results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// business logic
 var leapYear = function(year) {
   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
@@ -7,12 +8,19 @@ var leapYear = function(year) {
   }
 };
 
-
+//user interface logic
 $(document).ready(function() {
   $("form#leap-year").submit(function() {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+    $(".year").text(year);
+    if (!result) {
+      $(".not").text("not");
+    }
+    else {
+      $(".not").text("");
+    }
+    $("#result").show();
   });
 });
